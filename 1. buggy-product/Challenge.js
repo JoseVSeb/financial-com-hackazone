60975955d89a5a3622a99d1bd30524f0fa744953
Jose V Sebastian <josevseb99@gmail.com>
2024-03-02 11:47:28 +0530

    feat: challenges 1 & 2

--- 1. buggy-product/Challenge.js
+++ 1. buggy-product/Challenge.js
@@ -2,8 +2,24 @@
 // '0' indicates the version is bug-free and '1' indicates the version is buggy.
 // (Ex - For input [0, 0, 1, 1, 1], the bug was introduced in version 2 and the function should return 1)
 
+/**
+ *
+ * @param {number[]} versions
+ * @returns {number} last bug-free version
+ */
 function lastBugFreeVersion(versions) {
-  // Your implementation here
+  for (let i = 0; i < versions.length; i++) {
+    // current version is bug free
+    if (versions[i] === 0) continue;
+
+    // if all versions had bug, NOTE: outside question criteria
+    if (i === 0) throw new Error("All versions have bug.");
+
+    // current version has bug, last bug free version is the previous version
+    return i - 1;
+  }
+  // all versions are bug free, NOTE: outside question criteria
+  throw new Error("Bug does not exist.");
 }
 
-lastBugFreeVersion([0, 0, 1, 1, 1]);
+console.log(lastBugFreeVersion([0, 0, 1, 1, 1]));
--- 3. cheapest-flights/Solution.js
+++ 3. cheapest-flights/Solution.js
@@ -1,15 +1,108 @@
+/**
+ *
+ * @param {number} n total number of cities
+ * @param {[number,number,number][]} flights flight chart [from_i, to_i, price_i]
+ * @param {number} src source city
+ * @param {number} dst destination city
+ * @param {number} k max stops in between
+ * @returns {number} cheapest price
+ */
 function findCheapestPrice(n, flights, src, dst, k) {
-    // add your solution here
+  // construct a flight chart from flight list
+  const flightChart = flights.reduce(
+    (
+      /** @type {Map<number, Map<number, number>>} */
+      map,
+      [from_i, to_i, price_i]
+    ) => {
+      /** @type {Map<number, number>} */
+      const destinationPriceMap = map.get(from_i) ?? new Map();
+      map.set(from_i, destinationPriceMap);
+
+      destinationPriceMap.set(to_i, price_i);
+
+      return map;
+    },
+    new Map()
+  );
+
+  return getCheapestPrice(src, 0);
+
+  // TODO: memoize price map??
+
+  /**
+   *
+   * @param {number} i start
+   * @param {number} j end
+   * @param {number} stops stops
+   */
+  function getCheapestPrice(i, stops) {
+    const priceMap = flightChart.get(i);
+
+    // no route available
+    if (!priceMap) return -1;
+
+    // if destination available, last flight
+    if (priceMap.has(dst)) return priceMap.get(dst);
+
+    // max stops passed
+    if (stops === k) return -1;
+
+    let cheapestPrice = -1;
+    for (const [j, currentPrice] of priceMap) {
+      const nextFlight = getCheapestPrice(j, stops + 1);
+
+      // no routes found
+      if (nextFlight === -1) continue;
+
+      const totalPrice = currentPrice + nextFlight;
+
+      if (cheapestPrice === -1) {
+        cheapestPrice = totalPrice;
+        continue;
+      }
+
+      if (cheapestPrice <= totalPrice) continue;
+
+      cheapestPrice = totalPrice;
+    }
+
+    return cheapestPrice;
+  }
 }
 
 // Test case 1
-const flights1 = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]];
-console.log("Actual: " + findCheapestPrice(4, flights1, 0, 3, 1) + ", Expected: 700");
+const flights1 = [
+  [0, 1, 100],
+  [1, 2, 100],
+  [2, 0, 100],
+  [1, 3, 600],
+  [2, 3, 200],
+];
+console.log(
+  "Actual: " + findCheapestPrice(4, flights1, 0, 3, 1) + ", Expected: 700"
+);
 
 // Test case 2
-const flights2 = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]];
-console.log("Actual: " + findCheapestPrice(4, flights2, 0, 3, 0) + ", Expected: -1");
+const flights2 = [
+  [0, 1, 100],
+  [1, 2, 100],
+  [2, 0, 100],
+  [1, 3, 600],
+  [2, 3, 200],
+];
+console.log(
+  "Actual: " + findCheapestPrice(4, flights2, 0, 3, 0) + ", Expected: -1"
+);
 
 // Test case 3
-const flights3 = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]];
-console.log("Actual: " + findCheapestPrice(3, flights3, 0, 2, 0) + ", Expected: 500");
+const flights3 = [
+  [0, 1, 100],
+  [1, 2, 100],
+  [2, 0, 100],
+  [1, 3, 600],
+  [2, 3, 200],
+];
+console.log(
+  "Actual: " + findCheapestPrice(3, flights3, 0, 2, 0) + ", Expected: 500"
+);
